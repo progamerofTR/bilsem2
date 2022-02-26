f250e5703e322291cfca162a435a4cd408021bf7
progamerofTR <92209669+progamerofTR@users.noreply.github.com>
2022-02-26 13:48:16 +0300

    Add files via upload

---
+++ fonksiyon uygulama 2.py
@@ -0,0 +1,17 @@
+#kendisine girilen sayının karesinin karesini hesaplasın.
+
+sayi=int(input("Sayı gir:"))
+
+def hesapla(a):
+    print(a*a*a*a)
+hesapla(sayi)
+
+#girilen bu sayının çift mi tek mi olduğunu ekrana yazsın.
+
+def tekmi(b):
+    if b%2 == 0:
+        print("Bu sayı çift bir sayıdır.")
+    else:
+        print("Bu sayı tek bir sayıdır.")
+tekmi(sayi)
+
---
+++ fonksiyon uygulama.py
@@ -0,0 +1,6 @@
+#girilen sayının 3 katının 5 eksiğini hesaplayan programı fonksiyon kullanarak yapınız.
+
+sayi=int(input("Sayı girin :"))
+def hesapla(a):
+    print(sayi*3-5)
+hesapla(sayi)
