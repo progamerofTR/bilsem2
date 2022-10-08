5cc79e699ff4b9ce2168cda917977f498fcfaab2
progamerofTR <92209669+progamerofTR@users.noreply.github.com>
2022-10-08 15:38:27 +0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int a=10;
+	int b=3,c=4;
+	int d;
+	printf("a: %d, b: %d, c: %d \n", a,b,c);
+	printf("Bir sayi giriniz:");
+	scanf("%d", &d);
+	printf("\ngirilen sayi : %d\n",d);
+}
---
+++ Untitled2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int a, b;
+	printf("Birinci sayiyi giriniz: \n");
+	scanf("%d", &a);
+	printf("Ýkinci sayiyi giriniz: \n");
+	scanf("%d", &b);
+	printf("\ntoplam sayi: a+b=%d \n",a+b);
+}
